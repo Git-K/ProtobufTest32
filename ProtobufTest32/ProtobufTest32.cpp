9e290fefb1841744b80de49e2059778391acbf0f
YH Q <1516751431@qq.com>
2021-06-26 15:09:44 +0800

    cpp protocol buffer test

---
+++ ProtobufTest32/ProtobufTest32.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "hello.pb.h"
+using namespace std;
+
+int main() {
+	tutorial::Hello hi;
+	hi.set_say("Hello World");
+
+	// 序列化
+	string str;
+	hi.SerializeToString(&str);
+	cout << "序列化后：" << str << endl;
+	
+	// 反序列化
+	tutorial::Hello ha;
+	if (!ha.ParseFromString(str))
+	{
+		cerr << "failed." << endl;
+		return -1;
+	}
+	cout << "反序列化：" << ha.say() << endl;
+
+	google::protobuf::ShutdownProtobufLibrary();
+	return 0;
+}
